6f2b89afe18dddd41cdb53ec7acf5d8c5f4c3400
Leelavathi Gade <leelaroshangade@gmail.com>
2021-07-26 17:38:02 +0200

    new features added

--- RECIPES/script.js
+++ RECIPES/script.js
@@ -1,5 +1,57 @@
 
 
+   const viewBtn = document.querySelector('.share-more');
+   const  popup = document.querySelector('.popup');
+  const  close = popup.querySelector('.close');
+  
+  
+  
+   viewBtn.onclick =()=>{
+       popup.classList.toggle("show");
+   }
+  
+   close.onclick = ()=>{
+       viewBtn.click();
+   }
+  
+  
+
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+
+mybutton.onclick= ()=> {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -21,11 +73,38 @@
 
 
 
-let shareButton = document.getElementById('share-button');
+// let shareButton = document.getElementById('share-button');
  
-shareButton.onclick = ()=>{
+// shareButton.onclick = ()=>{
 
-    // alert('share this recipe with your friends');
+   
+
+//     // var element = document.getElementById("popup-div");
+//     // element.classList.toggle("popup-div");
+     
+
+//     alert( `share this link with your friends` );
+   
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     // document.getElementById('share-div').appendChild();
+ // alert('share this recipe with your friends');
 
     // let textbox = document.createElement('div');
 
@@ -34,17 +113,6 @@ shareButton.onclick = ()=>{
     // textbox.src = " "
     
     // textbox.innerHTML = 'share this link';
-
-    
-     
-    document.getElementById('share-div').appendChild();
-  
-
-    }
-
-
-
-
 
 //     shareButton.id = "newsharediv";
 // shareButton.innerHTML = "share this recipe link ";
